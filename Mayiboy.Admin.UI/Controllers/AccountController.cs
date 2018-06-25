0391de0485d4752b4e79c96293fd34cd90674f51
caimeng2009@126.com <caimeng2009@126.com>
2018-06-25 14:26:03 +0800

    optimize Error Login

--- Mayiboy.Admin.UI/Controllers/AccountController.cs
+++ Mayiboy.Admin.UI/Controllers/AccountController.cs
@@ -12,62 +12,75 @@ using Mayiboy.Utils;
 
 namespace Mayiboy.Admin.UI.Controllers
 {
-    public class AccountController : Controller
-    {
-        private readonly IUserInfoService _iuserinfoservice;
-        private readonly ISystemOperationLogService _systemOperationLogService;
+	public class AccountController : Controller
+	{
+		private readonly IUserInfoService _iuserinfoservice;
+		private readonly ISystemOperationLogService _systemOperationLogService;
 
-        public AccountController(IUserInfoService iuserinfoservice, ISystemOperationLogService systemOperationLogService)
-        {
-            _iuserinfoservice = iuserinfoservice;
-            _systemOperationLogService = systemOperationLogService;
-        }
 
-        // GET: Account
-        public ActionResult Index()
-        {
-            return View();
-        }
+		public AccountController(IUserInfoService iuserinfoservice, ISystemOperationLogService systemOperationLogService)
+		{
+			_iuserinfoservice = iuserinfoservice;
+			_systemOperationLogService = systemOperationLogService;
+		}
 
-        #region 用户登录
-        public ActionResult Login(LoginUserInfoModel model)
-        {
-            try
-            {
-                #region 验证验证码
-                var vcode = SessionHelper.Get<string>("vcode");
-                if (vcode.IsNullOrEmpty() || vcode != model.Code)
-                {
-                    return Json(new { status = 1, msg = "验证码错误" });
-                }
-                SessionHelper.Remove("vcode");
-                #endregion
+		// GET: Account
+		public ActionResult Index()
+		{
+			return View();
+		}
 
-                var request = new LoginQueryRequest
-                {
-                    LoginName = model.UserName,
-                    Password = model.PassWord.GetMd5()
-                };
+		#region 用户登录
+		public ActionResult Login(LoginUserInfoModel model)
+		{
+			try
+			{
+				#region 验证验证码登陆错误次数
+				var loginkey = model.UserName + RequestHelper.Ip;
+				var loginnum = int.Parse(CacheManager.RunTimeCache.Get(loginkey) ?? "0");
 
-                var loginqueryresponse = _iuserinfoservice.LoginQuery(request);
+				if (loginnum >= ConfigHelper.GetConfigInt("MaxNumberErrorLogin"))
+				{
+					return Json(new { status = 5, msg = "错误登陆次数超过上限" }, JsonRequestBehavior.AllowGet);
+				}
+				#endregion
 
-                if (loginqueryresponse.UserInfoEntity == null)
-                {
-                    return Json(new { status = 2, msg = "密码错误" }, JsonRequestBehavior.AllowGet);
-                }
+				#region 验证验证码
+				var vcode = SessionHelper.Get<string>("vcode");
+				if (vcode.IsNullOrEmpty() || vcode != model.Code)
+				{
+					return Json(new { status = 1, msg = "验证码错误" });
+				}
+				SessionHelper.Remove("vcode");
+				#endregion
 
-                #region 保存用户登录状态
-                string identityValue = Guid.NewGuid().ToString("N");
+				var request = new LoginQueryRequest
+				{
+					LoginName = model.UserName,
+					Password = model.PassWord.GetMd5()
+				};
 
-                CookieHelper.Set(PublicConst.IdentityCookieKey, identityValue, true);
+				var loginqueryresponse = _iuserinfoservice.LoginQuery(request);
 
-                var entity = loginqueryresponse.UserInfoEntity.As<AccountModel>();
+				if (loginqueryresponse.UserInfoEntity == null)
+				{
+					//记录ip地址、用户名登陆次数
+					CacheManager.RunTimeCache.Set(loginkey, (loginnum + 1).ToString(), PublicConst.Time.Day1);
+					return Json(new { status = 2, msg = "密码错误" }, JsonRequestBehavior.AllowGet);
+				}
 
-                entity.Fingerprint = RequestHelper.Fingerprint;
+				#region 保存用户登录状态
+				string identityValue = Guid.NewGuid().ToString("N");
 
-                var key = identityValue.AddCachePrefix(PublicConst.IdentityCookieKey);
+				CookieHelper.Set(PublicConst.IdentityCookieKey, identityValue, true);
 
-                CacheManager.RedisDefault.Set(key, entity, PublicConst.Time.Hour1);
+				var entity = loginqueryresponse.UserInfoEntity.As<AccountModel>();
+
+				entity.Fingerprint = RequestHelper.Fingerprint;
+
+				var key = identityValue.AddCachePrefix(PublicConst.IdentityCookieKey);
+
+				CacheManager.RedisDefault.Set(key, entity, PublicConst.Time.Hour1);
 
 				#region 只允许一个客户端登录
 				//var k = entity.Id.ToString().AddCachePrefix("LoginBind");
@@ -84,105 +97,105 @@ namespace Mayiboy.Admin.UI.Controllers
 
 				#region 记录用户操作日志
 				_systemOperationLogService.AddOperationLog(new AddOperationLogRequest
-                {
-                    Content = string.Format("[LoginName:{0}]-[Name:{1}]-[Content:{2}]", entity.LoginName, entity.Name, "用户登录")
-                });
-                #endregion
+				{
+					Content = string.Format("[LoginName:{0}]-[Name:{1}]-[Content:{2}]", entity.LoginName, entity.Name, "用户登录")
+				});
+				#endregion
 
-                #endregion
+				#endregion
 
-                return Json(new { status = 0 }, JsonRequestBehavior.AllowGet);
-            }
-            catch (Exception ex)
-            {
-                LogManager.DefaultLogger.ErrorFormat("登录出错：{0}", new { model, err = ex.ToString() }.ToJson());
-                return Json(new { status = -1, msg = "系统出错！" }, JsonRequestBehavior.AllowGet);
-            }
-        }
-        #endregion
+				return Json(new { status = 0 }, JsonRequestBehavior.AllowGet);
+			}
+			catch (Exception ex)
+			{
+				LogManager.DefaultLogger.ErrorFormat("登录出错：{0}", new { model, err = ex.ToString() }.ToJson());
+				return Json(new { status = -1, msg = "系统出错！" }, JsonRequestBehavior.AllowGet);
+			}
+		}
+		#endregion
 
-        #region 注销登录
-        [LoginAuth]
-        [OperLog("注销登录")]
-        public ActionResult Logout()
-        {
-            SessionHelper.Clear();
+		#region 注销登录
+		[LoginAuth]
+		[OperLog("注销登录")]
+		public ActionResult Logout()
+		{
+			SessionHelper.Clear();
 
-            //删除登录信息
-            var keyl = CookieHelper.Get(PublicConst.IdentityCookieKey).AddCachePrefix(PublicConst.IdentityCookieKey);
-            CacheManager.RedisDefault.Del(keyl);
+			//删除登录信息
+			var keyl = CookieHelper.Get(PublicConst.IdentityCookieKey).AddCachePrefix(PublicConst.IdentityCookieKey);
+			CacheManager.RedisDefault.Del(keyl);
 
-            CookieHelper.Remove(PublicConst.IdentityCookieKey);
+			CookieHelper.Remove(PublicConst.IdentityCookieKey);
 
-            //删除权限
-            var keyp = LoginAccount.Identity.AddCachePrefix("userpermission");
-            CacheManager.RedisDefault.Del(keyp);
+			//删除权限
+			var keyp = LoginAccount.Identity.AddCachePrefix("userpermission");
+			CacheManager.RedisDefault.Del(keyp);
 
-            return Redirect("Index");
-        }
-        #endregion
+			return Redirect("Index");
+		}
+		#endregion
 
-        #region 修改用户密码
-        /// <summary>
-        /// 修改用户密码
-        /// </summary>
-        /// <returns></returns>
-        [LoginAuth]
-        [OperLog("修改用户密码")]
-        public ActionResult ChangePassword(string oldpwd, string newpwd)
-        {
-            try
-            {
-                var response = _iuserinfoservice.ChangePassword(new ChangePasswordRequest
-                {
-                    UserId = LoginAccount.UserInfo.Id,
-                    OldPassword = oldpwd,
-                    NewPassword = newpwd
-                });
+		#region 修改用户密码
+		/// <summary>
+		/// 修改用户密码
+		/// </summary>
+		/// <returns></returns>
+		[LoginAuth]
+		[OperLog("修改用户密码")]
+		public ActionResult ChangePassword(string oldpwd, string newpwd)
+		{
+			try
+			{
+				var response = _iuserinfoservice.ChangePassword(new ChangePasswordRequest
+				{
+					UserId = LoginAccount.UserInfo.Id,
+					OldPassword = oldpwd,
+					NewPassword = newpwd
+				});
 
-                if (!response.IsSuccess)
-                {
-                    return Json(new { status = 1, msg = response.MessageText }, JsonRequestBehavior.AllowGet);
-                }
+				if (!response.IsSuccess)
+				{
+					return Json(new { status = 1, msg = response.MessageText }, JsonRequestBehavior.AllowGet);
+				}
 
-                return Json(new { status = 0 }, JsonRequestBehavior.AllowGet);
-            }
-            catch (Exception ex)
-            {
-                LogManager.DefaultLogger.ErrorFormat("修改用户密码出错{0}", new { oldpwd, newpwd, err = ex.ToString() }.ToJson());
-                return Json(new { status = -1, msg = "系统出错！" }, JsonRequestBehavior.AllowGet);
-            }
+				return Json(new { status = 0 }, JsonRequestBehavior.AllowGet);
+			}
+			catch (Exception ex)
+			{
+				LogManager.DefaultLogger.ErrorFormat("修改用户密码出错{0}", new { oldpwd, newpwd, err = ex.ToString() }.ToJson());
+				return Json(new { status = -1, msg = "系统出错！" }, JsonRequestBehavior.AllowGet);
+			}
 
-        }
-        #endregion
+		}
+		#endregion
 
-        #region 验证码
-        /// <summary>
-        /// 验证码
-        /// </summary>
-        /// <returns></returns>
-        public ActionResult VerifyCode()
-        {
-            string vcode = CaptchaHelper.CreateRandomCode(4);
+		#region 验证码
+		/// <summary>
+		/// 验证码
+		/// </summary>
+		/// <returns></returns>
+		public ActionResult VerifyCode()
+		{
+			string vcode = CaptchaHelper.CreateRandomCode(4);
 
-            SessionHelper.Set("vcode", vcode);
+			SessionHelper.Set("vcode", vcode);
 
-            return File(CaptchaHelper.DrawImage(vcode), @"image/jpeg");
-        }
-        #endregion
+			return File(CaptchaHelper.DrawImage(vcode), @"image/jpeg");
+		}
+		#endregion
 
-        //用户信息页面
-        public ActionResult UserInfoPage()
-        {
-            return View();
-        }
+		//用户信息页面
+		public ActionResult UserInfoPage()
+		{
+			return View();
+		}
 
 
-        //保存用户基本信息
-        public ActionResult SaveUserInfo()
-        {
-            return Json(new { }, JsonRequestBehavior.AllowGet);
-        }
+		//保存用户基本信息
+		public ActionResult SaveUserInfo()
+		{
+			return Json(new { }, JsonRequestBehavior.AllowGet);
+		}
 
-    }
+	}
 }
