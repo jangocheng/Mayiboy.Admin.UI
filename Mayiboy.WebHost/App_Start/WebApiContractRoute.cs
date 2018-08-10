306583afed7d58f2f84cc0f8371332f0e5964565
Simon <caimeng2009@126.com>
2018-08-10 21:55:05 +0800

    optimize

---
+++ Mayiboy.WebHost/App_Start/WebApiContractRoute.cs
@@ -0,0 +1,89 @@
+﻿using System.Web.Http;
+using Framework.Mayiboy.Ioc;
+using Framework.Mayiboy.MvcExtensions.Routes;
+using Mayiboy.ConstDefine;
+using Mayiboy.Contract;
+using Mayiboy.Contract.Contracts;
+using Mayiboy.Utils;
+
+namespace Mayiboy.WebHost
+{
+	public class WebApiContractRoute
+	{
+		private static readonly object Queryappidauth = new object();
+
+		/// <summary>
+		/// 绑定路由
+		/// </summary>
+		/// <param name="config"></param>
+		public static void Bind(HttpConfiguration config)
+		{
+			var entryRoute = new ApiContractRoute();
+
+			//entryRoute.ExpiresTime = 10000;
+			//entryRoute.IsBind = true;
+
+			entryRoute.OnDecryptParameter = QueryDecryptParameter;
+
+			//用户信息接口
+
+
+			config.Routes.Add("singleEntryRoute", entryRoute);
+		}
+
+		/// <summary>
+		/// 获取解密参数
+		/// </summary>
+		/// <param name="appid">应用标识</param>
+		/// <param name="authtoken">授权Token</param>
+		/// <param name="encryptionType">加密类型（0：不加密；1：对称加密（DES）；2：对称加密（AES）；3：非对称加密</param>
+		/// <param name="secretKey">解密秘钥</param>
+		private static void QueryDecryptParameter(string appid, ref string authtoken, ref int encryptionType, ref string secretKey)
+		{
+			if (!string.IsNullOrEmpty(appid))
+			{
+				var key = appid.AddCachePrefix("AppIdAuth");
+
+				var entity = CacheManager.Get<AppIdAuthDto>(key, 60);
+
+				if (entity == null)
+				{
+					lock (Queryappidauth)
+					{
+						entity = CacheManager.Get<AppIdAuthDto>(key, 60);
+
+						if (entity == null)
+						{
+							var response = ServiceLocater.GetService<IAppIdAuthService>().QueryByAppId(new QueryByAppIdRequest { ServiceAppId = appid });
+
+							if (response.IsSuccess && response.Entity != null)
+							{
+								entity = response.Entity;
+
+								CacheManager.RedisDefault.Set(key, entity, PublicConst.Time.Hour2);
+							}
+						}
+					}
+				}
+
+				if (entity != null)
+				{
+					authtoken = entity.AuthToken;
+					encryptionType = entity.EncryptionType;
+
+					switch (entity.EncryptionType)
+					{
+						case 1:
+						case 2:
+							secretKey = entity.SecretKey;
+							break;
+						case 3:
+							secretKey = entity.PrivateKey;
+							break;
+					}
+				}
+			}
+		}
+
+	}
+}
--- Mayiboy.WebHost/Exception/AuthorizationHandler.cs
+++ Mayiboy.WebHost/Exception/AuthorizationHandler.cs
@@ -1,6 +1,16 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
 using System.Threading.Tasks;
+using Framework.Mayiboy.Ioc;
+using Framework.Mayiboy.Utility;
+using Mayiboy.ConstDefine;
+using Mayiboy.Contract;
+using Mayiboy.Utils;
 
 namespace Mayiboy.WebHost.Exception
 {
@@ -8,18 +18,126 @@ namespace Mayiboy.WebHost.Exception
     {
         protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, System.Threading.CancellationToken cancellationToken)
         {
-          
-            return base.SendAsync(request, cancellationToken);
-        }
+			var headerappid = GetValue(request.Headers, "appid");
+			var headertoken = GetValue(request.Headers, "authorizationtoken");
+			var headertimestamp = GetValue(request.Headers, "timestamp");
+			var headersign = GetValue(request.Headers, "sign");
+			var headerunauthorized = GetValue(request.Headers, "Unauthorized");
 
+			//验证必要参数
+			if (string.IsNullOrEmpty(headerappid) || string.IsNullOrEmpty(headertoken))
+			{
+				return Unauthorized();
+			}
 
-        /// <summary>
-        /// 非法访问
-        /// </summary>
-        /// <returns></returns>
-        private Task<HttpResponseMessage> Unauthorized()
-        {
-            return null;
-        }
+			//校验授权
+			if (GetAppIdToken(headerappid) != headertoken)
+			{
+				return Unauthorized();
+			}
+
+			//校验接口有效期(接口10分钟后无效)
+			if (string.IsNullOrEmpty(headertimestamp) || long.Parse(headertimestamp).ToDateTime() < DateTime.Now.AddMinutes(-10))
+			{
+				return Unauthorized();
+			}
+
+			return base.SendAsync(request, cancellationToken);
+		}
+
+		/// <summary>
+		/// 获取值
+		/// </summary>
+		/// <param name="requestheaders"></param>
+		/// <param name="name"></param>
+		/// <returns></returns>
+		private string GetValue(HttpRequestHeaders requestheaders, string name)
+		{
+			IEnumerable<string> values;
+
+			if (requestheaders.TryGetValues(name, out values))
+			{
+				return values.First();
+			}
+
+			return "";
+		}
+
+		/// <summary>
+		/// 非法访问
+		/// </summary>
+		/// <returns></returns>
+		private Task<HttpResponseMessage> Unauthorized()
+		{
+			//如果没有授权则返回403错误
+			return Task.Factory.StartNew<HttpResponseMessage>(() =>
+			{
+				return new HttpResponseMessage(HttpStatusCode.Forbidden);
+			});
+		}
+
+		/// <summary>
+		/// 获取应用标识下的token
+		/// </summary>
+		/// <param name="appid"></param>
+		/// <returns></returns>
+		private string GetAppIdToken(string appid)
+		{
+			if (string.IsNullOrEmpty(appid)) return "";
+
+			var key = appid.AddCachePrefix("ServiceToken");
+
+			var token = CacheManager.Get(key, 60);
+
+			if (token.IsNullOrEmpty())
+			{
+				var response = ServiceLocater.GetService<IAppIdAuthService>().QueryByAppId(new QueryByAppIdRequest
+				{
+					ServiceAppId = appid
+				});
+
+				if (response.IsSuccess && response.Entity != null)
+				{
+					token = response.Entity.AuthToken;
+
+					if (token.IsNotNullOrEmpty())
+					{
+						CacheManager.RedisDefault.Set(key, token, PublicConst.Time.Hour2);
+					}
+				}
+			}
+
+			return token;
+		}
+
+		/// <summary>
+		/// 获取 Gign
+		/// </summary>
+		/// <param name="appid"></param>
+		/// <param name="accessToken"></param>
+		/// <param name="content"></param>
+		/// <param name="timestamp"></param>
+		/// <returns></returns>
+		private string GetSign(string appid, string accessToken, string content, string timestamp)
+		{
+			StringBuilder sb = new StringBuilder();
+
+			Dictionary<string, string> stringADict = new Dictionary<string, string>();
+
+			stringADict.Add("appid", appid);
+			stringADict.Add("authorizationtoken", accessToken);
+			stringADict.Add("contentstring", content);
+			stringADict.Add("time", timestamp);
+
+			foreach (var item in stringADict.OrderBy(x => x.Key)) //参数名ASCII码从小到大排序（字典序）；
+			{
+				sb.Append(item.Key).Append("=").Append(item.Value).Append("&");
+			}
+
+			var sign = sb.ToString().GetMd5().ToLower();//对stringSignTemp进行MD5运算，再将得到的字符串所有字符转换为小写，得到sign值signValue。 
+			return sign;
+
+		}
+
     }
 }
